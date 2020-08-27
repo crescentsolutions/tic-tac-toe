75c4450b7ff2d6528373d42028ee2eec9147c2dc
daylinjones <crescentwd@gmail.com>
2020-08-27 15:29:05 -0400

    Change Password
    Received assistance and change pw now works. The objects had to be
    fixed.

--- curl-scripts/auth/sign-out.sh
+++ curl-scripts/auth/sign-out.sh
@@ -1,16 +1,11 @@
 # Source: https://git.generalassemb.ly/daylinjones/jquery-ajax-token-auth
 # Source: https://git.generalassemb.ly/ga-wdi-boston/library-api/blob/master/docs/authors.md
+# Source: https://git.generalassemb.ly/ga-wdi-boston/game-project-api/blob/master/docs/user.md#signout
 
 curl "https://tic-tac-toe-api-production.herokuapp.com/change-password" \
   --include \
   --request PATCH \
   --header "Authorization: Bearer ${TOKEN}" \
   --header "Content-Type: application/json" \
-  --data '{
-    "passwords": {
-    "old": "'"${OLDPW}"'",
-    "new": "'"${NEWPW}"'"
-  }
-}'
 
 echo
