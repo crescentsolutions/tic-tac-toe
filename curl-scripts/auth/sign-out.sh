8dfcf25e725e6ee86038e69832e6d65f7a85adb2
daylinjones <crescentwd@gmail.com>
2020-08-27 15:45:52 -0400

    Preparing to test game API with curl requests

--- curl-scripts/auth/sign-out.sh
+++ curl-scripts/auth/sign-out.sh
@@ -2,9 +2,9 @@
 # Source: https://git.generalassemb.ly/ga-wdi-boston/library-api/blob/master/docs/authors.md
 # Source: https://git.generalassemb.ly/ga-wdi-boston/game-project-api/blob/master/docs/user.md#signout
 
-curl "https://tic-tac-toe-api-production.herokuapp.com/change-password" \
+curl "https://tic-tac-toe-api-production.herokuapp.com/sign-out" \
   --include \
-  --request PATCH \
+  --request DELETE \
   --header "Authorization: Bearer ${TOKEN}" \
   --header "Content-Type: application/json" \
 
