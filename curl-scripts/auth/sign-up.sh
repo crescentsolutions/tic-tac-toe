b1ccd22f4d7bee6b2ed1b4b9db8ad695ae246fc3
daylinjones <crescentwd@gmail.com>
2020-08-27 14:33:27 -0400

    Sign In
    Confirmed sign in API works through curl request test in the terminal

---
+++ curl-scripts/auth/sign-up.sh
@@ -0,0 +1,16 @@
+# Source: https://git.generalassemb.ly/daylinjones/jquery-ajax-token-auth
+# Source: https://git.generalassemb.ly/ga-wdi-boston/library-api/blob/master/docs/authors.md
+
+curl "https://tic-tac-toe-api-production.herokuapp.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
