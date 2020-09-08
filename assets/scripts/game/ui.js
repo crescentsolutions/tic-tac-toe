92489a9e2a3ccff916893fec59e8a81560296cff
daylinjones <crescentwd@gmail.com>
2020-09-08 16:39:32 -0400

    Updating game ui

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -26,52 +26,53 @@ const gameTrackingSuccess = function (response) {
   gameStore.id = response.game._id
   $('#message').text('Move has been tracked')
 
-  const gameCells = response.game.cells
+  const gameCells = response.game.cells.filter('')
   const winningMessage = function () {
     $('#message').text('YOU ARE THE WINNER!!!')
     $('#message').css('font-size', '42px')
     $('#game-board').hide()
   }
-  if (k) {
-    if (gameCells[0] === gameCells[1] && gameCells[0] === gameCells[2]) {
-      winningMessage()
-      console.log('winning scenario 1')
 
-      // middle row winner
-    }  //  else if (gameCells[3] === gameCells[4] && gameCells[3] === gameCells[5]) {
-   //   winningMessage()
-   //   console.log('winning scenario 2')
-   //
-   //   // bottom row
-   // }
+  // const noBlankCells = gameCells.
+
+  if (gameCells[0] === gameCells[1] && gameCells[0] === gameCells[2]) {
+    winningMessage()
+    console.log('winning scenario 1')
+
+    // middle row winner
+  } else if (gameCells[3] === gameCells[4] && gameCells[3] === gameCells[5]) {
+    winningMessage()
+    console.log('winning scenario 2')
+    // bottom row
   }
-
- // else if (gameCells[6] === gameCells[7] && gameCells[6] === gameCells[8]) {
-  //   winningMessage()
-  //
-  //   // left column
-  // } else if (gameCells[0] === gameCells[3] && gameCells[0] === gameCells[6]) {
-  //   winningMessage()
-  //
-  //   // middle column
-  // } else if (gameCells[1] === gameCells[4] && gameCells[1] === gameCells[7]) {
-  //   winningMessage()
-  //
-  //   // last column
-  // } else if (gameCells[2] === gameCells[5] && gameCells[2] === gameCells[8]) {
-  //   winningMessage()
-  //
-  //   // diagonal going right
-  // } else if (gameCells[0] === gameCells[4] && gameCells[0] === gameCells[8]) {
-  //   winningMessage()
-  //
-  //   // diagonal going left
-  // } else if (gameCells[2] === gameCells[4] && gameCells[2] === gameCells[6]) {
-  //   winningMessage()
-  // } else {
-  //   $('#message').text('Tie Game')
-  // }
+  // End of gameTrackingSuccess
 }
+
+// else if (gameCells[6] === gameCells[7] && gameCells[6] === gameCells[8]) {
+//   winningMessage()
+//   // left column
+// } else if (gameCells[0] === gameCells[3] && gameCells[0] === gameCells[6]) {
+//   winningMessage()
+//
+//   // middle column
+// } else if (gameCells[1] === gameCells[4] && gameCells[1] === gameCells[7]) {
+//   winningMessage()
+//
+//   // last column
+// } else if (gameCells[2] === gameCells[5] && gameCells[2] === gameCells[8]) {
+//   winningMessage()
+//
+//   // diagonal going right
+// } else if (gameCells[0] === gameCells[4] && gameCells[0] === gameCells[8]) {
+//   winningMessage()
+//
+//   // diagonal going left
+// } else if (gameCells[2] === gameCells[4] && gameCells[2] === gameCells[6]) {
+//   winningMessage()
+// } else {
+//   $('#message').text('Tie Game')
+// }
+
 const gameTrackingFailure = function (error) {
   $('#message').text('Could not track game play')
   console.log('Could not track game play')
